44acedf8c1354f89867ffa70a6e9a1ef683fe476
mreddy <mahesh.reddy@ivanti.com>
2024-07-04 13:42:49 +0530

    second sql Statement 2

--- dbscripts/always_scripts/A__GrantRole_Permission.sql
+++ dbscripts/always_scripts/A__GrantRole_Permission.sql
@@ -1,6 +1,4 @@
 
-create user TESTUSER;
-create role testrole;
 grant usage on database demo to role testrole;
 grant usage on future schemas in database demo to role testrole;
 grant select on future tables in database demo to role testrole;
