62b9d7b414fbbd6e40b5d8f905ef056a31b783f6
mreddy <mahesh.reddy@ivanti.com>
2024-07-04 13:30:21 +0530

    Second SQL Commited

---
+++ dbscripts/versioned_scripts/V1.1.2__Second_Statement.sql
@@ -0,0 +1,8 @@
+
+USE SCHEMA DEMO;
+
+insert into STUDENT values(1,'john smith','computer science');
+insert into STUDENT values(2,'robert junior','mechanical');
+
+
+alter table BRANCH add column strength integer;
